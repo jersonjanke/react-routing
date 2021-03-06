e6d490cd78a5969907a47ac5a66b19b765a2b55f
jerson janke <jersonjanke@gmail.com>
2021-03-06 16:13:20 -0300

    Adding React Router to your application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./Home";
+import About from "./About";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <main>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+      </main>
+    </Router>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <serction>
+      <h1>Welcome to the weather report</h1>
+      <p>
+        Please select on of the options below to view the current weather in
+        your temperature scale of choise!
+      </p>
+    </serction>
+  );
+};
+
+export default Home;
